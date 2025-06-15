8c0936f5a3bd7d5b07171736a7152ecec98c7f07
Lazuli R <130087792+wishgender@users.noreply.github.com>
2025-06-15 18:34:55 -0400

    updated pokemon by dex number to have sprites

--- scripts/getByDex.js
+++ scripts/getByDex.js
@@ -1,7 +1,15 @@
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
-
+function checkDate() {
+    // now = Date(Date.now()).slice(4,10)
+    const d = new Date();
+    if (d.getMonth()==7 && d.getDate()==3) return true;
+    else if (d.getMonth()==4 && d.getDate()==31) return true;
+    else if (d.getMonth()==9 && d.getDate()==27) return true;
+    else if (d.getMonth()==0 && d.getDate()==2) return true;
+    else return false;
+}
 function getPokemon() {
     const dexNumber = document.getElementById('dexInput').value;
     const result = document.getElementById('result');
@@ -15,7 +23,14 @@ function getPokemon() {
         return response.json();
     })
     .then(data => {
-        result.textContent = `Pokémon #${dexNumber} is ${capitalize(data.name)}`;
+        result.textContent = `Pokémon #${dexNumber} is ${capitalize(data.species.name)}`;
+        pic = new Image()
+        if (checkDate())
+            pic.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${dexNumber}.png`;
+        else
+            pic.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${dexNumber}.png`;
+        result.style = 'text-align:center;display:flex'
+        result.appendChild(pic)
     })
     .catch(error => {
         result.textContent = error.message;
