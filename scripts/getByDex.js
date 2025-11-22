9e92108118a93a705ec18d6ddc51b0b781d44fe1
Lazuli R <130087792+wishgender@users.noreply.github.com>
2025-11-22 11:50:36 -0500

    fixed leading zero issue and erroneous shiny date

--- scripts/getByDex.js
+++ scripts/getByDex.js
@@ -4,15 +4,16 @@ function capitalize(str) {
 function checkDate() {
     // now = Date(Date.now()).slice(4,10)
     const d = new Date();
-    if (d.getMonth()==7 && d.getDate()==3) return true;
-    else if (d.getMonth()==4 && d.getDate()==31) return true;
-    else if (d.getMonth()==9 && d.getDate()==27) return true;
-    else if (d.getMonth()==0 && d.getDate()==2) return true;
-    else if (d.getMonth()==10 && d.getDate()==14) return true;
+    if (d.getMonth()+1==8 && d.getDate()==3)         return true;
+    else if (d.getMonth()+1==1 && d.getDate()==2)    return true;
+    else if (d.getMonth()+1==5 && d.getDate()==9)    return true;
+    else if (d.getMonth()+1==5 && d.getDate()==31)   return true;
+    else if (d.getMonth()+1==9 && d.getDate()==14)   return true;
+    else if (d.getMonth()+1==10 && d.getDate()==27)  return true;
     else return false;
 }
 function getPokemon() {
-    const dexNumber = document.getElementById('dexInput').value;
+    let dexNumber = Number(document.getElementById('dexInput').value);
     const result = document.getElementById('DexResult');
     const caption = document.getElementById('ResultText');
     // Clear previous result
